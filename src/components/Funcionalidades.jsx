8f6b14ee6a2359f43bdf7355cdbd79a465888766
vitor-1fernandes <vitorfernandes11122006@gmail.com>
2025-09-23 22:12:55 -0300

    criação dos cards de funcionalidade

--- src/components/Funcionalidades.jsx
+++ src/components/Funcionalidades.jsx
@@ -1,8 +1,61 @@
-export default function Funcionalidades () {
-    return (
-        <div className="bg-white grid-cols-3 justify-center items-center h-200">
-            <div className="w-full h-10 bg-gradient-to-r from-purple-600 to-blue-600"></div>
-            <h1 className="flex justify-center items-center grid-span-3">Desenvolvemos com carinho</h1>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDisplay } from "@fortawesome/free-solid-svg-icons";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+
+export default function Funcionalidades() {
+  return (
+    <div className="bg-[#111111] pr-5 pl-5 pt-2 grid grid-cols-3 justify-center w-[100%] items-start h-auto">
+      <div className="flex flex-col justify-start items-center h-100 col-span-3 pt-5 pl-10 pr-10 gap-10">
+        <h1 className="flex text-5xl justify-center items-center col-span-3 font-bold text-white">
+          Desenvolvemos com carinho
+        </h1>
+        <div className="flex flex-row gap-20">
+          <div
+            className="transition-all ease-in-out duration-500 hover:scale-105 flex flex-col justify-center items-center bg-gradient-to-r from-[#190dc5] to-[#4f34c9]  rounded-lg h-65 w-60 text-white"
+            style={{ boxShadow: "1px 0px 10px 1px rgba(52, 58, 146, 0.582)" }}
+          >
+            <h1 className="mb-2 text-2xl">Feito pra você </h1>
+            <div className="text-[70px]">
+              <FontAwesomeIcon icon={faHandHoldingHeart} />
+            </div>
+
+            <h3 className="w-[80%] text-center text-lg">
+              Recomendações de novos artistas e playlists personalizadas
+            </h3>
+          </div>
+
+          <div
+            className="transition-all ease-in-out duration-500 hover:scale-105 flex flex-col justify-center items-center bg-[#4f34c9] rounded-lg h-65 w-60 text-white"
+            style={{ boxShadow: "1px 0px 10px 1px rgba(93, 52, 146, 0.582)" }}
+          >
+            <h1 className="mb-2 text-2xl">Responsividade </h1>
+            <div className="text-[70px]">
+              <FontAwesomeIcon icon={faDisplay} />
+            </div>
+
+            <h3 className="w-[80%] text-center text-lg">
+              Design adaptado para diferentes dispositivos
+            </h3>
+          </div>
+
+
+          <div
+            className="transition-all ease-in-out duration-500 hover:scale-105 flex flex-col justify-center items-center rounded-lg h-65 w-60 bg-gradient-to-l from-[#190dc5] to-[#4f34c9] text-white"
+            style={{ boxShadow: "1px 0px 10px 1px rgba(52, 58, 146, 0.582)" }}
+          >
+            <h1 className="mb-2 text-2xl">Sinta sua Vibe</h1>
+            <div className="text-[70px]">
+              <FontAwesomeIcon icon={faMusic} />
+            </div>
+
+            <h3 className="w-[80%] text-center text-lg">
+              Qualidade de som superior
+            </h3>
+          </div>
+
         </div>
-    )
+      </div>
+    </div>
+  );
 }

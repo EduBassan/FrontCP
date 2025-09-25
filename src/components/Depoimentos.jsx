402e876e0e37b916ecbd72116913dd006342e9df
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-09-25 01:39:10 -0300

    ajuste na responsividade dos depoimentos

--- src/components/Depoimentos.jsx
+++ src/components/Depoimentos.jsx
@@ -8,18 +8,19 @@ import "swiper/css/pagination";
 import modeloRoxo from "../img/modelo_roxo.PNG";
 import modeloAzul from "../img/modelo_azul.PNG";
 import modeloBranco from "../img/modelo_branco.PNG";
+import modeloCinza from "../img/modelo_cinza.PNG";
 
 
 export default function Depoimentos() {
   const testimonials = [
-    { img: modeloRoxo, name: "Marcelo Azevedo", text: "Muito bom!" },
-    { img: modeloBranco, name: "Carla Lima", text: "Voltarei sempre." },
-    { img: modeloAzul, name: "Arthur Macedo", text: "Excelente experiência!" },
-    { img: modeloBranco, name: "Carla Lima", text: "Voltarei sempre." }
+    { img: modeloRoxo, name: "Marcelo Azevedo", text: "Semprei procurei por um aplicativo prático" },
+    { img: modeloCinza, name: "Carla Lima", text: "Nota 10! Recomendo para todos" },
+    { img: modeloAzul, name: "Arthur Macedo", text: "Minha playlist personalizada é a melhor!" },
+    { img: modeloBranco, name: "Carla Lima", text: "Adorei o design, combina com meu estilo" }
   ];
 
   return (
-    <div className="bg-[#111111] overflow-hidden w-auto h-auto flex flex-col items-center pt-1 pb-1 px-1">
+    <div className="overflow-hidden w-auto h-auto flex flex-col justify-center items-center mb-20 lg:mb-0">
     <style>{`
       .swiper-button-next,
       .swiper-button-prev {
@@ -33,7 +34,7 @@ export default function Depoimentos() {
         font-size: 20px;
       }
     `}</style>
-      <h1 className="text-3xl lg:text-5xl font-bold text-white text-center">
+      <h1 className="text-2xl lg:text-4xl font-bold text-white text-center">
         Nossas avaliações
       </h1>
 
@@ -43,10 +44,11 @@ export default function Depoimentos() {
         slidesPerView={1}
         navigation={true}
         loop={true}
-        className="w-90 lg:w-250 mt-10"
+        className="w-90 md:w-100 lg:180 xl:w-210 mt-6"
         breakpoints={{
           640: { slidesPerView: 1 } ,
-          1024: { slidesPerView: 3 },
+          1200: { slidesPerView: 1 },
+          1600: { slidesPerView: 3 }
         }}
       >
         {testimonials.map((t, i) => (
@@ -55,10 +57,10 @@ export default function Depoimentos() {
               <div className="w-50 h-50 overflow-hidden rounded-full border-4 border-white">
                 <img src={t.img} alt={t.name} className="w-full" />
               </div>
-              <h3 className="w-[80%] text-center text-lg mt-2 font-semibold text-white">
+              <h3 className="w-[80%] text-center text-xl mt-2 font-semibold text-white">
                 {t.name}
               </h3>
-              <h3 className="w-[80%] text-center text-lg font-thin text-white">
+              <h3 className="w-[80%] text-center text-xl font-thin text-white">
                 {t.text}
               </h3>
             </div>

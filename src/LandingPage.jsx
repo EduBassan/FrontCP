24664785a8e8ab7f0d0008752bf2a9a61814090e
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-09-26 00:08:49 -0300

    alteração no bg

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -8,12 +8,11 @@ import NavBar from "./components/NavBar";
 
 export default function LandingPage () {
     return (
-        <div className="bg-gradient-to-b from-[#190dc5] to-[#7434c9] h-full overflow-hidden w-full">
+        <div className="h-full overflow-hidden w-full">
             <NavBar/>
             <Hero/>
             <Apresentacao/>
             <Funcionalidades/>
-            <Forms/>
             <Footer/>
         </div>
     )
